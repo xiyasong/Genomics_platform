f306286ee60f341c1ed7c3c1d759ca0fdac3be7c
xiyasong <67383094+xiyasong@users.noreply.github.com>
2023-11-22 18:04:20 +0100

    Update ACMG_findings_and_whole_clinvar.R
    sorted_tab_SW_sillico
    sorted_tab_TR_sillico

--- R_scripts/ACMG_findings_and_whole_clinvar.R
+++ R_scripts/ACMG_findings_and_whole_clinvar.R
@@ -46,17 +46,10 @@ customize_temp_data <- function(data) {
   return(data)
 }
 
-# Function to get the pathogenic variants plus HIGH impact LoFs that suits for writing, to differ with reporting files ------------------
+# Function to get the pathogenic variants that suits for writing, to differ with reporting files ------------------
 get_P_LP_LoFs <- function(data){
   data <- data %>% filter(X.CHROM != 'chrM') %>% filter(MAX_AF < 0.05 | is.na(MAX_AF)) %>%
-  filter(ClinVar_CLNSIG == 'Pathogenic' | ClinVar_CLNSIG == 'Likely_pathogenic' | IMPACT == "HIGH")
-  return(data)
-}
-
-# Function to get the pathogenic variants that suits for writing, to differ with reporting files ------------------
-get_P_LP <- function(data){
-  data <- data %>% filter(X.CHROM != 'chrM') %>% filter(MAX_AF < 0.05 | is.na(MAX_AF)) %>%
-  filter(ClinVar_CLNSIG == 'Pathogenic' | ClinVar_CLNSIG == 'Likely_pathogenic')
+    filter(ClinVar_CLNSIG == 'Pathogenic' | ClinVar_CLNSIG == 'Likely_pathogenic' | IMPACT == "HIGH")
   return(data)
 }
 
@@ -113,6 +106,7 @@ get_sorted_tab <- function(data, unique_data) {
   
   return(list(sorted_tab = sorted_tab, sorted_tab_old = sorted_tab_old))
 }
+
 # Process Turkish data -----------------
 df_temp_turkish <- read_and_process_files("/Users/xiyas/V2_Genome_reporting/python_output_turkish_275/nodup_4_file", "Turkish")
 df_temp_turkish <- customize_temp_data(df_temp_turkish)
@@ -126,7 +120,16 @@ sillico_pLoFs_unique_TR <- get_unique_variants(sillico_pLoFs)
 ACMG_TR <- get_ACMG_findings(df_temp_turkish)
 ACMG_TR_unique <- get_unique_SZAID(ACMG_TR)
 
+# clinVar variants 
+## before the P_LP filteration
+## now sorted_tab_TR = cohort-level unique SZAID clinvar_TR with freq count
 sorted_tab_TR <- get_sorted_tab(clinvar_TR, clinvar_TR_unique)
+
+sorted_tab_TR_filter =list(sorted_tab_filter = get_P_LP(sorted_tab_TR$sorted_tab),
+                           sorted_tab_old_filter = get_P_LP(sorted_tab_TR$sorted_tab_old))
+
+# sillico variants
+sorted_tab_TR_sillico <- get_sorted_tab(sillico_pLoFs_TR, sillico_pLoFs_unique_TR)
 
 # Process Swedish data -----------------
 df_temp_swedish <- read_and_process_files("/Users/xiyas/V2_Genome_reporting/python_output_swedish_101/nodup4_file", "Swedish")
@@ -141,7 +144,14 @@ sillico_pLoFs_unique_SW <- get_unique_variants(sillico_pLoFs_SW)
 ACMG_SW <- get_ACMG_findings(df_temp_swedish)
 ACMG_SW_unique <- get_unique_SZAID(ACMG_SW)
 
+# clinVar variants  
+## before the P_LP filteration
 sorted_tab_SW <- get_sorted_tab(clinvar_SW, clinvar_SW_unique)
+## after the P_LP filteration
+sorted_tab_SW_filter =list(sorted_tab_filter = get_P_LP(sorted_tab_SW$sorted_tab),
+                           sorted_tab_old_filter = get_P_LP(sorted_tab_SW$sorted_tab_old))
+# sillico variants
+sorted_tab_SW_sillico <- get_sorted_tab(sillico_pLoFs_SW, sillico_pLoFs_unique_SW)
 
 # Combine the two data frames into one -----------------
 combined_df <- rbind(clinvar_TR,clinvar_SW)
