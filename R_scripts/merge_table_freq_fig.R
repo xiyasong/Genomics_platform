e9e704ba27daa3a7d082a048e9ae50cb7fcfc2de
xiyasong <67383094+xiyasong@users.noreply.github.com>
2024-01-25 14:17:08 +0100

    Update merge_table_freq_fig.R

--- R_scripts/merge_table_freq_fig.R
+++ R_scripts/merge_table_freq_fig.R
@@ -5,15 +5,47 @@ library(tidytext)
 library(R.utils)
 library(ggsci)
 library(ggplot2)
-dev.off()
 library(patchwork)
 
+dev.off()
 # Reading files =============
-#setwd('/Users/xiyas/pgsc_calc/vcf_turkish-with-merge')
-setwd('/Users/xiyas/pgsc_calc/vcf-swedish-with-merge')
-#var_info <- fread('function_biallelic-275-samples-Merged-add-ref-parameter.vcf.gz_vep_annotated.vcf.rm.missing.gz',header = FALSE,sep=' ')
-var_info <- fread('function_biallelic-101-samples-Merged-add-ref-parameter.vcf.gz_vep_annotated.vcf.rm.missing.gz',header = FALSE,sep=' ')
-var_info <-distinct(var_info)
+# reading original files after the cohort-level-processing on Linux ---------------
+cohort = "SW"
+
+if (cohort == "TR") {
+  # Process 1
+  Database_type <- fread('/Users/xiyas/V2_Genome_reporting/data/SZAID_biallelic-275-samples-Merged-add-ref.txt.gz',
+                         header = FALSE,sep=' ')
+  var_info <- fread('/Users/xiyas/V2_Genome_reporting/data/function_biallelic-275-samples-Merged-add-ref-parameter.vcf.gz_vep_annotated.vcf.rm.missing.gz',header = FALSE,sep=' ')
+  freq <- fread('/Users/xiyas/V2_Genome_reporting/data/freq_biallelic.rm.missing.frq.gz')
+  
+} else if (cohort == "SW") {
+  # Process 2
+  Database_type <- fread('/Users/xiyas/V2_Genome_reporting/data/SZAID_biallelic-101-samples-Merged-add-ref.txt.gz',
+                         header = FALSE,sep=' ')
+  var_info <- fread('/Users/xiyas/V2_Genome_reporting/data/function_biallelic-101-samples-Merged-add-ref-parameter.vcf.gz_vep_annotated.vcf.rm.missing.gz',header = FALSE,sep=' ')
+  freq <- fread('/Users/xiyas/V2_Genome_reporting/data/freq_biallelic_rm_missing_101.frq.gz')
+} else {
+  print("set which cohort")
+}
+
+# processing two df and adding the category  ---------------
+
+freq_unique <- distinct(freq) # Use an appropriate summary function
+var_info_distinct <-distinct(var_info)
+
+colnames(freq) <- c('CHROM','POS','N_ALLELS','N_CHR','REF_AF','ALT_AF')
+colnames(Database_type) <- c('CHROM','POS','REF','ALT','db_type','SZAID')
+cols <- c("CHROM", "POS", "REF", "ALT")
+Database_type[, Variant_info := do.call(paste, c(.SD, sep = "-")), .SDcols = cols]
+#substring_to_search <- "ClinP_LP_var"
+
+# Filter rows where "ClinVar_P_LP" is found in "Variant_info"
+#filtered_data <- Database_type %>%
+#  filter(grepl("ClinP_LP_var", db_type))
+#table(filtered_data$Variant_info)
+
+var_info_distinct <-distinct(var_info)
 colnames(var_info) <- c('CHROM','POS','REF','ALT','rs_id','Consequence','type','IMPACT','MAX_AF','MAX_AF_POPS')
 var_info$Consequence <- sapply(strsplit(var_info$Consequence,"&"), `[`, 1)
 var_info <- var_info %>%
@@ -31,17 +63,6 @@ var_info <- var_info %>%
   ))
 
 
-#custom_colors <- c("No_public AF" = "orange2", "Public AF < 0.05" = "chartreuse4", "Public AF >= 0.05" = "royalblue3")
-custom_colors <- c("deletion" = "tomato1", "SNV" = "chartreuse4", "insertion" = "royalblue3")
-legend_labels <- c("insertion" = "INS", "deletion" = "DEL", 'SNV' = 'SNV')
-
-## Get a copy dataframe ============
-var_info_test <- var_info
-
-########Frequency plot. Figure 4A ===============
-#freq <- fread('freq_biallelic.rm.missing.frq.gz')
-freq <- fread('freq_biallelic_rm_missing_101.frq.gz')
-colnames(freq) <- c('CHROM','POS','N_ALLELS','N_CHR','REF_AF','ALT_AF')
 freq <- freq %>%
   mutate(
     REF = sapply(strsplit(REF_AF, ":", fixed = TRUE), `[`, 1),
@@ -49,53 +70,67 @@ freq <- freq %>%
     ALT = sapply(strsplit(ALT_AF, ":", fixed = TRUE), `[`, 1),
     ALT_AF = as.numeric(sapply(strsplit(ALT_AF, ":", fixed = TRUE), `[`, 2))
   )
+#custom_colors <- c("No_public AF" = "orange2", "Public AF < 0.05" = "chartreuse4", "Public AF >= 0.05" = "royalblue3")
+#custom_colors <- c("deletion" = "tomato1", "SNV" = "chartreuse4", "insertion" = "royalblue3")
+legend_labels <- c("insertion" = "INS", "deletion" = "DEL", 'SNV' = 'SNV')
 
+## Get a copy dataframe ============
+var_info_test <- var_info
+
+
+
+########Frequency plot. Figure 4A ===============
+var_info <- distinct(var_info)
+# generate merge_table_ori and AF lables ==============
 merge_table_ori <-merge(freq,var_info,all=TRUE,by = c('CHROM','POS','REF','ALT'))
-
 
 # Define the conditions and group labels ==============
 ## Turkish ==============
-merge_table_ori <- merge_table_ori %>%
-  mutate(group = case_when(
-    ALT_AF %in% c(0.00181818) ~ "AC =1",
-    ALT_AF %in% c(0.00363636) ~ "AC=2",
-    ALT_AF %in% c(0.00545455) ~ "AC=3",
-    ALT_AF %in% c(0.00727273) ~ "AC=4",
-    ALT_AF %in% c(0.00909091) ~ "AC=5",
-    (ALT_AF > 0.01 & ALT_AF <= 0.05) ~ "0.01<AF<=0.05",
-    (ALT_AF > 0.05 & ALT_AF <= 0.15) ~ "0.05<AF<=0.15",
-    (ALT_AF > 0.15 & ALT_AF <= 0.25) ~ "0.15<=AF<=0.25",
-    (ALT_AF > 0.25 & ALT_AF <= 0.35) ~ "0.25<=AF<=0.35",
-    (ALT_AF > 0.35 & ALT_AF <= 0.45) ~ "0.35<=AF<=0.45",
-    (ALT_AF > 0.45 & ALT_AF <= 0.55) ~ "0.45<=AF<=0.55",
-    (ALT_AF > 0.55 & ALT_AF <= 0.65) ~ "0.55<=AF<=0.65",
-    (ALT_AF > 0.65 & ALT_AF <= 0.75) ~ "0.65<=AF<=0.75",
-    (ALT_AF > 0.75 & ALT_AF <= 0.85) ~ "0.75<=AF<=0.85",
-    (ALT_AF > 0.85 & ALT_AF <= 0.95) ~ "0.85<=AF<=0.95",
-    (ALT_AF > 0.95 & ALT_AF < 1) ~ "0.95<AF<1",
-    ALT_AF %in% c(1) ~ "AF=1",
-    TRUE ~ "Other"
-  )) 
+if (cohort == "TR") {
+  merge_table_ori <- merge_table_ori%>%
+    mutate(group = case_when(
+      ALT_AF %in% c(0.00181818) ~ "AC =1",
+      ALT_AF %in% c(0.00363636) ~ "AC=2",
+      ALT_AF %in% c(0.00545455) ~ "AC=3",
+      ALT_AF %in% c(0.00727273) ~ "AC=4",
+      ALT_AF %in% c(0.00909091) ~ "AC=5",
+      (ALT_AF > 0.01 & ALT_AF <= 0.05) ~ "0.01<AF<=0.05",
+      (ALT_AF > 0.05 & ALT_AF <= 0.15) ~ "0.05<AF<=0.15",
+      (ALT_AF > 0.15 & ALT_AF <= 0.25) ~ "0.15<=AF<=0.25",
+      (ALT_AF > 0.25 & ALT_AF <= 0.35) ~ "0.25<=AF<=0.35",
+      (ALT_AF > 0.35 & ALT_AF <= 0.45) ~ "0.35<=AF<=0.45",
+      (ALT_AF > 0.45 & ALT_AF <= 0.55) ~ "0.45<=AF<=0.55",
+      (ALT_AF > 0.55 & ALT_AF <= 0.65) ~ "0.55<=AF<=0.65",
+      (ALT_AF > 0.65 & ALT_AF <= 0.75) ~ "0.65<=AF<=0.75",
+      (ALT_AF > 0.75 & ALT_AF <= 0.85) ~ "0.75<=AF<=0.85",
+      (ALT_AF > 0.85 & ALT_AF <= 0.95) ~ "0.85<=AF<=0.95",
+      (ALT_AF > 0.95 & ALT_AF < 1) ~ "0.95<AF<1",
+      ALT_AF %in% c(1) ~ "AF=1",
+      TRUE ~ "Other"
+    )) 
+} else if (cohort == "SW") {
+  merge_table_ori <- merge_table_ori %>%
+    mutate(group = case_when(
+      ALT_AF %in% c(0.0049505) ~ "AC =1",
+      ALT_AF %in% c(0.00990099) ~ "AC=2",
+      (ALT_AF > 0.01 & ALT_AF <= 0.05) ~ "0.01<AF<=0.05",
+      (ALT_AF > 0.05 & ALT_AF <= 0.15) ~ "0.05<AF<=0.15",
+      (ALT_AF > 0.15 & ALT_AF <= 0.25) ~ "0.15<=AF<=0.25",
+      (ALT_AF > 0.25 & ALT_AF <= 0.35) ~ "0.25<=AF<=0.35",
+      (ALT_AF > 0.35 & ALT_AF <= 0.45) ~ "0.35<=AF<=0.45",
+      (ALT_AF > 0.45 & ALT_AF <= 0.55) ~ "0.45<=AF<=0.55",
+      (ALT_AF > 0.55 & ALT_AF <= 0.65) ~ "0.55<=AF<=0.65",
+      (ALT_AF > 0.65 & ALT_AF <= 0.75) ~ "0.65<=AF<=0.75",
+      (ALT_AF > 0.75 & ALT_AF <= 0.85) ~ "0.75<=AF<=0.85",
+      (ALT_AF > 0.85 & ALT_AF <= 0.95) ~ "0.85<=AF<=0.95",
+      (ALT_AF > 0.95 & ALT_AF < 1) ~ "0.95<AF<1",
+      ALT_AF %in% c(1) ~ "AF=1",
+      TRUE ~ "Other"
+    ))
+} else {
+  print("set which cohort")
+}
 
-## Swedish ==============
-merge_table_ori <- merge_table_ori %>%
-  mutate(group = case_when(
-    ALT_AF %in% c(0.0049505) ~ "AC =1",
-    ALT_AF %in% c(0.00990099) ~ "AC=2",
-    (ALT_AF > 0.01 & ALT_AF <= 0.05) ~ "0.01<AF<=0.05",
-    (ALT_AF > 0.05 & ALT_AF <= 0.15) ~ "0.05<AF<=0.15",
-    (ALT_AF > 0.15 & ALT_AF <= 0.25) ~ "0.15<=AF<=0.25",
-    (ALT_AF > 0.25 & ALT_AF <= 0.35) ~ "0.25<=AF<=0.35",
-    (ALT_AF > 0.35 & ALT_AF <= 0.45) ~ "0.35<=AF<=0.45",
-    (ALT_AF > 0.45 & ALT_AF <= 0.55) ~ "0.45<=AF<=0.55",
-    (ALT_AF > 0.55 & ALT_AF <= 0.65) ~ "0.55<=AF<=0.65",
-    (ALT_AF > 0.65 & ALT_AF <= 0.75) ~ "0.65<=AF<=0.75",
-    (ALT_AF > 0.75 & ALT_AF <= 0.85) ~ "0.75<=AF<=0.85",
-    (ALT_AF > 0.85 & ALT_AF <= 0.95) ~ "0.85<=AF<=0.95",
-    (ALT_AF > 0.95 & ALT_AF < 1) ~ "0.95<AF<1",
-    ALT_AF %in% c(1) ~ "AF=1",
-    TRUE ~ "Other"
-  ))
 desired_levels <- c(
   "AC =1", "AC=2", "AC=3","AC=4","AC=5",
   "0.01<AF<=0.05", "0.05<AF<=0.15", "0.15<=AF<=0.25",
@@ -105,22 +140,62 @@ desired_levels <- c(
 # Reorder the "group" variable using the desired levels
 merge_table_ori$group <- factor(merge_table_ori$group, levels = desired_levels)
 
-# get merge_table =================
+# get merge_table, which is only autosome region variants =================
 ### remove chrX and chrY , because the allele frequency calculation weird
-merge_table <- merge_table_ori[merge_table_ori$CHROM != 'chrX' & merge_table_ori$CHROM != 'chrY' ,]
+merge_table <- merge_table_ori[merge_table_ori$CHROM != 'chrX' & merge_table_ori$CHROM != 'chrY',]
 merge_table$MAX_AF_Category <- factor(merge_table$MAX_AF_Category, levels = c(
-  "Public MAX_AF >= 0.05","0.01 < Public MAX_AF < 0.05", "Public MAX_AF < 0.01", "No public MAX_AF"
+  "No public MAX_AF","Public MAX_AF < 0.01","0.01 < Public MAX_AF < 0.05","Public MAX_AF >= 0.05"
 ))
-# Modify specific legend labels
-#legend_labels <- c("insertion" = "INS", "deletion" = "DEL", 'SNV' = 'SNV')
-#write.table(merge_table, file= 'merge_table.txt',quote = FALSE,sep = '\t', row.names = FALSE)
-write.table(merge_table, file= 'merge_table_swe.txt',quote = FALSE,sep = '\t', row.names = FALSE)
 
-# Figure 4A ggplot ============
+#grouped_df$MAX_AF_Category <- factor(grouped_df$MAX_AF_Category, levels = 
+### Table 1 --------------------------
+#final_merge <- var_info %>%
+#  left_join(freq,by=c('CHROM','POS','REF','ALT')) %>%  left_join(Database_type, by=c('CHROM','POS','REF','ALT'))
+#head(final_merge)
+table(merge_table_ori$group)
+table(merge_table_ori$MAX_AF_Category)
+
+table(merge_table$group)
+table(merge_table$MAX_AF_Category)
+
+table(merge_table_ori[merge_table_ori$rs_id_category == "Novel",]$group)
+table(merge_table_ori[merge_table_ori$rs_id_category == "Novel",]$MAX_AF_Category)
+# Modify specific legend labels =================
+#legend_labels <- c("insertion" = "INS", "deletion" = "DEL", 'SNV' = 'SNV')
+
+# save merge_table =================
+#write.table(merge_table, file= 'merge_table.txt',quote = FALSE,sep = '\t', row.names = FALSE)
+#write.table(merge_table, file= 'merge_table_swe.txt',quote = FALSE,sep = '\t', row.names = FALSE)
+
+# Figure 3A and supplementary Figure ggplot ============
+
+cohort = "SW"
+if (cohort == "TR") {
+  # Process 1
+  merge_table <- fread('/Users/xiyas/V2_Genome_reporting/data/merge_table.txt',header = TRUE)
+} else if (cohort == "SW") {
+  # Process 2
+  merge_table <- fread('/Users/xiyas/V2_Genome_reporting/data/merge_table_swe.txt',header = TRUE)
+} else {
+  print("set which cohort")
+}
+
+merge_table$MAX_AF_Category <- factor(merge_table$MAX_AF_Category, levels = c(
+  "No public MAX_AF","Public MAX_AF < 0.01","0.01 < Public MAX_AF < 0.05","Public MAX_AF >= 0.05"
+))
+desired_levels <- c(
+  "AC =1", "AC=2", "AC=3","AC=4","AC=5",
+  "0.01<AF<=0.05", "0.05<AF<=0.15", "0.15<=AF<=0.25",
+  "0.25<=AF<=0.35", "0.35<=AF<=0.45", "0.45<=AF<=0.55", "0.55<=AF<=0.65",
+  "0.65<=AF<=0.75", "0.75<=AF<=0.85", "0.85<=AF<=0.95", "0.95<AF<1", "AF=1","Other"
+)
+# Reorder the "group" variable using the desired levels
+merge_table$group <- factor(merge_table$group, levels = desired_levels)
+
 ggplot(merge_table, aes(x = group,fill=MAX_AF_Category)) +
   geom_bar(aes(y = ..count.. / 1000000)) +
-  labs(x = "Allele Frequency of Turkish cohort", y = "No.of variants (10^6)") +
-  scale_fill_jco()+
+  labs(x = "Allele Frequency of Swedish cohort", y = "No.of variants (10^6)") +
+  scale_fill_nejm()+
   #scale_fill_manual(labels = legend_labels) +
   facet_wrap(~ rs_id_category) +
   theme_minimal()+
@@ -141,12 +216,12 @@ ggplot(merge_table, aes(x = group,fill=MAX_AF_Category)) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   coord_flip()
 
+dev.off()
+#ggsave("/Users/xiyas/V2_Genome_reporting/Plots/Allele_frequencies_Figure3A.pdf",height = 10,width =20)
+ggsave("/Users/xiyas/V2_Genome_reporting/Plots/Allele_frequencies_Figure3A_swe.pdf",height = 10,width =20)
 
-#ggsave("/Users/xiyas/V2_Genome_reporting/Plots/Allele_frequencies_Figure4A.pdf",height = 10,width =20)
-ggsave("/Users/xiyas/V2_Genome_reporting/Plots/Allele_frequencies_Figure4A_swe.pdf",height = 10,width =20)
 
-
-# Figure 4B ===========
+# Figure 3B ===========
 
 ori_order <- var_info_test %>%
   group_by(Consequence) %>%
@@ -205,7 +280,7 @@ consequence_counts$conse_group <- factor(consequence_counts$conse_group, levels 
 re_consequence_proportions$conse_group <- factor(re_consequence_proportions$conse_group, levels =unique(order$conse_group))
 re_MAX_proportions$conse_group <- factor(re_MAX_proportions$conse_group, levels =unique(order$conse_group))
 
-# Figure 4B ggplot ============
+# Figure 3B ggplot ============
 # Create the facet grid plot with two separate facets for counts and Novel ratios
 gg_1 <- ggplot(consequence_counts, aes(x = conse_group, y = Count)) +
   geom_bar(stat = 'identity',fill= "mediumpurple3") +
